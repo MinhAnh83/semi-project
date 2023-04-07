5f6da38ecc656b4ce938c9df5ffc30c7a8f91af9
MinhAnh83 <anhtlmbs00225@fpt.edu.vn>
2023-04-07 13:20:59 +0700

    update3

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Contact;
 use App\Entity\Review;
 use App\Form\ReviewType;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+
 
 class ContactController extends AbstractController
 {
